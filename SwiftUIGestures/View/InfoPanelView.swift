4222c3b951708fd6f1be3e10b799dacefd82fccb
noramari <noora@selinan.com>
2023-05-07 16:53:50 +0900

    add infopanelview

---
+++ SwiftUIGestures/View/InfoPanelView.swift
@@ -0,0 +1,57 @@
+//
+//  InfoPanelView.swift
+//  SwiftUIGestures
+//
+//  Created by Noora Maeda on 2023/05/07.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+    var scale: CGFloat
+    var offset: CGSize
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "circle.circle")
+                .symbolRenderingMode(.hierarchical)
+                .resizable()
+                .frame(width: 30, height: 30)
+            
+            Spacer()
+            
+            HStack(spacing: 2) {
+                Image(systemName: "arrow.up.left.and.arrow.down.right")
+                Text("\(scale)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.left.and.arrow.right")
+                Text("\(offset.width)")
+                
+                Spacer()
+                
+                Image(systemName: "arrow.up.and.arrow.down")
+                Text("\(offset.height)")
+                
+                Spacer()
+            }
+            .font(.footnote)
+            .padding(8)
+            .background(.ultraThinMaterial)
+            .cornerRadius(8)
+            .frame(maxWidth: 420)
+            
+            Spacer()
+        }
+    }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoPanelView(scale: 1, offset: .zero)
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
