9810d3ed46dc7f7372a43ab4483d04285ac67357
noramari <noora@selinan.com>
2023-05-07 16:43:23 +0900

    add drag gesture

--- SwiftUIGestures/Screen/ContentView.swift
+++ SwiftUIGestures/Screen/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero // same as CGSize(width: 0, height: 0)
     
     var body: some View {
         NavigationView {
@@ -23,6 +24,7 @@ struct ContentView: View {
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
                     .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height) // drag
                     .scaleEffect(imageScale)
                     .onTapGesture(count: 2) {
                         if imageScale == 1 {
@@ -30,11 +32,22 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     }
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             }
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -43,6 +56,13 @@ struct ContentView: View {
             }
         }
         .navigationViewStyle(.stack)
+    }
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
     }
 }
 
