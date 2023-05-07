c5e776936980f08caed469521f3e595e4b72da4e
noramari <noora@selinan.com>
2023-05-07 15:41:47 +0900

    Initial Commit

---
+++ SwiftUIGestures/SwiftUIGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIGesturesApp.swift
+//  SwiftUIGestures
+//
+//  Created by Noora Maeda on 2023/05/07.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
