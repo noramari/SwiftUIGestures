da72286c406758cb370c6625e58f76fa47b031d9
noramari <noora@selinan.com>
2023-05-07 17:17:15 +0900

    add control panel

---
+++ SwiftUIGestures/View/ControlImageView.swift
@@ -0,0 +1,26 @@
+//
+//  ControlImageView.swift
+//  SwiftUIGestures
+//
+//  Created by Noora Maeda on 2023/05/07.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
